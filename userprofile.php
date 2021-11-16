b8a6ed06f06113fcdefb1e12b59ffd90ee17e075
Tanishqmalu <54908702+Tanishqmalu@users.noreply.github.com>
2021-11-17 00:35:14 +0530

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -1,10 +1,35 @@
 <?php
-include('auth.php');
+session_start();
+if(!isset($_SESSION['GardenerID'])) {
+    header("Location: gardenerlogin.php");
+    exit();
+}
 require('db.php');
-$var1 = $_SESSION['skid'];
-$sql = "SELECT * from `Shopkeeper` where skid= '$var1'" ;
+$var1 = $_SESSION['GardenerID'];
+$sql = "SELECT * from `gardener` where gardenerid= '$var1'" ;
 $query = mysqli_query($db, $sql);
 $row = mysqli_fetch_assoc($query);
+?>
+<?php
+$var1 = $_SESSION['GardenerID'];
+if (isset($_REQUEST['submit'])) {
+  $name = $_REQUEST['name'];
+  $gender = $_REQUEST['gender'];
+  $dob = $_REQUEST['dob'];
+  $contactno = $_REQUEST['contactno'];
+  $address = $_REQUEST['address'];
+  $doj = $_REQUEST['doj'];
+
+    $query    = "Update `gardener` SET Name = '$name', gender = '$gender', Address='$address',contactno = '$contactno', dob='$dob', doj = '$doj' WHERE GardenerID='$var1'";
+    $result = mysqli_query($db, $query);
+    $rows = mysqli_fetch_assoc($result);
+    echo '<script type = "text/javascript">';
+    echo 'alert("Changes Updated!");';
+    echo 'window.location.href = "userprofile.php"';
+    echo '</script>';
+
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -16,155 +41,98 @@ $row = mysqli_fetch_assoc($query);
   </head>
   <body>
     <div class="container">
-    <div class="main-body">
+		<div class="main-body">
+      <nav aria-label="breadcrumb" class="main-breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="">Update Profile</a></li>
+          <a href = "userprofile.php" class="ml-auto"> Back </a>
+        </ol>
+      </nav>
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="card">
+						<div class="card-body">
+							<div class="d-flex flex-column align-items-center text-center">
+                <?php if($row['gender']=='M'): ?>
+                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+              <?php else: ?>
+              <img src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="Admin" class="rounded-circle" width="150">
+            <?php endif; ?>
+                <div class="mt-3">
+                  <h4><?php echo $row['username']; ?></h4>
+                  <p class="text-secondary mb-1">Gardener ID</p>
+                  <p class="text-muted font-size-sm"><?php echo $row['GardenerID']; ?></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-8">
+					<div class="card">
+						<div class="card-body">
+              <form>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">Full Name</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" name="name" class="form-control" value="<?php echo trim($row['Name']);?>">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">Gender</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" name="gender" class="form-control" value="<?php echo $row['gender'];?>">
+								</div>
+							</div>
+              <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">Address</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" name="address" class="form-control" value="<?php echo $row['Address'];?>">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">Phone</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" name="contactno"  class="form-control" value="<?php echo $row['contactno'];?> ">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">Date Of Birth</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="date" name="dob" class="form-control" value=<?php echo $row['DoB'];?>>
+								</div>
+							</div>
+              <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0">Date Of Joining</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="date" name="doj" class="form-control" value=<?php echo $row['DoJ'];?>>
+								</div>
+							</div>
 
-          <!-- Breadcrumb -->
-          <nav aria-label="breadcrumb" class="main-breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="">User profile</a></li>
-              <a href = "logout.php" class="ml-auto"> Logout </a>
-            </ol>
+							<div class="row">
+								<div class="col-sm-3"></div>
+								<div class="col-sm-9 text-secondary">
+									<input type="submit" name="submit" class="btn btn-primary px-4" value="Save Changes">
+								</div>
+							</div>
+            </form>
+						</div>
+					</div>
 
-          </nav>
-          <!-- /Breadcrumb -->
-
-          <div class="row gutters-sm">
-            <div class="col-md-4 mb-3">
-              <div class="card h">
-                <div class="card-body">
-                  <div class="d-flex flex-column align-items-center text-center">
-                    <?php if($row['gender']=="M"){ ?>
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
-                  <?php }else{ ?>
-                  <img src="https://bootdey.com/img/Content/avatar/avatar8.png" alt="Admin" class="rounded-circle" width="150">
-                <?php } ?>
-                    <div class="mt-3">
-                      <h4><?php echo $row['skname']; ?></h4>
-                      <p class="text-secondary mb-1">Shopkeeper ID</p>
-                      <p class="text-muted font-size-sm"><?php echo $row['skid']; ?></p>
-                      <!-- <button class="btn btn-primary">Follow</button>
-                      <button class="btn btn-outline-primary">Message</button> -->
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- <div class="card mt-3">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                    <span class="text-secondary">https://bootdey.com</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
-                    <span class="text-secondary">bootdey</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
-                    <span class="text-secondary">@bootdey</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
-                    <span class="text-secondary">bootdey</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                    <span class="text-secondary">bootdey</span>
-                  </li>
-                </ul>
-              </div> -->
-            </div>
-            <div class="col-md-8">
-              <div class="card mb-3">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Full Name</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      <?php echo $row['skname']; ?>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Gender</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      <?php echo $row['gender']; ?>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Address</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      <?php echo $row['address']; ?>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Phone</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      <?php echo $row['contact']; ?>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Date of Birth</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      <?php echo $row['dob']; ?>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <a class="btn btn-info " href="profile_update.php">Update</a>
-                    </div>
-                    <div class="col-sm-6">
-                      <a class="btn btn-info "href="profile_password_update.php">Change Password</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="row gutters-sm">
-                <div class="col-sm-6 mb-3">
-                  <div class="card h-100">
-                    <div class="card-body">
-                      <h6 class="d-flex align-items-center mb-3">My Shop 1</h6>
-
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-6 mb-3">
-                  <div class="card h-100">
-                    <div class="card-body">
-                      <h6 class="d-flex align-items-center mb-3"> My Shop 2</h6>
-
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-
-
-            </div>
-          </div>
-
-        </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
   </body>
 </html>
