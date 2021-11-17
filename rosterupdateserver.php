f98f3b0cfa0000409b44c66bd4d7c824a9974809
Jenish Monpara <54908938+jenishmonpara@users.noreply.github.com>
2021-11-18 00:48:44 +0530

    Update rosterupdateserver.php

--- rosterupdateserver.php
+++ rosterupdateserver.php
@@ -5,50 +5,36 @@
         $gid = $_POST['gid'];
         $area = $_POST['area'];
         $date = $_POST['date'];
+        $table = "roster_".date("m_y");
 
         echo "<br/> $gid $area $date <br/>";
 
-        // checking if entry exists
-        // $sql = "SELECT count(*) FROM roster WHERE date = '$date' and area = '$area'";
-        // $result = mysqli_query($db, $sql);
-        // $row = mysqli_fetch_assoc($result);
-        // echo $result;
-        // // echo $row['count(*)'];
-        // if($row['count(*)'] === 0) {
-        //     echo "row inserted <br\>";
-        //     $sql = "insert into roster('date','area','gid') values ('$date','$area','$gid')";
-        //     $result = mysqli_query($db, $sql);
-        //     echo " status : " + $result + "<br/>";
-        // }
-        // else{
-        //     echo "row updated <br\>";
-        //     $sql = "update roster set gid = $gid where date = $date and area = $area";;
-        //     $result = mysqli_query($db, $sql);
-        //     echo " status : " + $result + "<br/>";
-        // }
+        $sql = "SELECT * FROM information_schema.tables where table_schema = 'TheSchema' and table_name = '$table'";
+        $result = mysqli_query($db, $sql);
+        if($result->num_rows == 0){
+            echo "\n creating table \n";
+            $sql = "create table {$table} (date date,area varchar(20),gid int)";
+            $result = mysqli_query($db, $sql);
+            echo mysqli_error($db);
+        }
 
-
-
-
-
-
-        $sql = "SELECT * FROM roster WHERE date = '$date' and area = '$area'";
-        echo "<br/> hi";
+        $sql = "SELECT * FROM {$table} WHERE date = '$date' and area = '$area'";
         $result = mysqli_query($db, $sql);
         $row = mysqli_fetch_assoc($result);
+        
         $cnt = mysqli_num_rows($result);
         echo "<br/> $cnt";
         if($cnt === 0) {
             echo $cnt;
             echo "row inserted <br\>";
-            $sql = "insert into roster(date,area,gid) values ('$date','$area','$gid')";
+            $sql = "insert into {$table}(date,area,gid) values ('$date','$area','$gid')";
             $result = mysqli_query($db, $sql);
             echo " status : $result <br/>";
         }
         else{
             echo $cnt;
             echo "row updated <br\>";
-            $sql = "update roster set gid = '$gid' where date = '$date' and area = '$area'";;
+            $sql = "update {$table} set gid = '$gid' where date = '$date' and area = '$area'";;
             $result = mysqli_query($db, $sql);
             echo " status : $result <br/>";
         }
